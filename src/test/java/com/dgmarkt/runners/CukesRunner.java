0d3dadf3faa12ba99d4cfe57c258ef45bd49ed41
Ayse3434 <aysegul2013x@gmail.com>
2023-11-15 13:51:02 +0100

    ilk commit

---
+++ src/test/java/com/dgmarkt/runners/CukesRunner.java
@@ -0,0 +1,23 @@
+package com.dgmarkt.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"json:target/cucumber.json",
+                "html:target/default-html-reports.html",
+                "rerun:target/rerun.txt"
+        },
+        features = "src/test/resources/currency.feature",
+        glue = "com/dgmarkt/step_definitions",
+        dryRun = false,
+        tags = ""
+
+)
+
+public class CukesRunner {
+
+
+
+}
